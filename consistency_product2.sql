bc82a80eda8f0572b0f3270d2e9e34c7a91c718d
magusafr <40602197+magusafr@users.noreply.github.com>
2022-09-25 20:16:23 +0700

    Update consistency_product2.sql

--- consistency_product2.sql
+++ consistency_product2.sql
@@ -1,31 +1,23 @@
 -- Consistency product 2
 SELECT *
 FROM iowa_drink_sales
-WHERE store_number IN (SELECT store_number 
+WHERE item_number IN (SELECT item_number 
 					 FROM (
-							SELECT 
-							  store_number,
-							  COUNT(DISTINCT(store_name)) AS total_store_name,
-							  COUNT(DISTINCT(address)) AS total_store_address,
-							  COUNT(DISTINCT(city)) AS total_store_address,
-							  COUNT(DISTINCT(zip_code)) AS total_store_zip_code,
-							  COUNT(DISTINCT(store_location)) AS total_store_location,
-							  COUNT(DISTINCT(county_number)) AS total_store_anumber,
-							  COUNT(DISTINCT(county)) AS total_store_county
-							FROM 
-								iowa_drink_sales
-							GROUP BY
-								store_number
-							HAVING 
-								COUNT(DISTINCT(store_name)) > 1 OR
-						 		COUNT(DISTINCT(address)) > 1 OR
-						 		COUNT(DISTINCT(city)) > 1 OR
-						 		COUNT(DISTINCT(zip_code)) > 1 OR
-						 		COUNT(DISTINCT(store_location)) > 1 OR
-						 		COUNT(DISTINCT(county_number)) > 1 OR
-						 		COUNT(DISTINCT(county)) > 1
-							ORDER BY 
-								total_store_name DESC, store_number
-								)A
-					)
-ORDER BY store_number;
+						SELECT 
+						  item_number,
+						  COUNT(DISTINCT(item_description)) AS total_item_description,
+						  COUNT(DISTINCT(category_name)) AS total_category_name,
+						  COUNT(DISTINCT(category)) AS total_category_number
+						FROM 
+							iowa_drink_sales
+						GROUP BY
+							item_number
+						HAVING 
+							COUNT(DISTINCT(item_description)) > 1 OR
+							COUNT(DISTINCT(category_name)) > 1 OR
+							COUNT(DISTINCT(category)) > 1
+						ORDER BY 
+							total_item_description DESC, item_number;
+						)A
+			)
+ORDER BY item_number;
