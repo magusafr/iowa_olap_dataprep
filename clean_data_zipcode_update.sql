f47b5597dac6720ca327298440742d36f1160b7a
magusafr <40602197+magusafr@users.noreply.github.com>
2022-09-25 20:32:24 +0700

    Update clean_data_zipcode_update.sql

--- clean_data_zipcode_update.sql
+++ clean_data_zipcode_update.sql
@@ -5,6 +5,6 @@ WHERE store_number = '5116'
 ORDER BY zip_code;
 
 -- update zip code store 5116 from 50702 to 50208
-UPDATE iowa_drink_sales 
-SET zip_code = '50208'
-WHERE store_name = '5116' AND zip_code = '50702';
+--UPDATE iowa_drink_sales 
+--SET zip_code = '50208'
+--WHERE store_name = '5116' AND zip_code = '50702';
