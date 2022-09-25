af353e5ef1038736c6d7bc28c0f1f1e2c0e5ab94
magusafr <40602197+magusafr@users.noreply.github.com>
2022-09-25 20:31:29 +0700

    Create clean_data_zipcode_update.sql

---
+++ clean_data_zipcode_update.sql
@@ -0,0 +1,10 @@
+-- fiter zip code 5116
+SELECT *
+FROM iowa_drink_sales
+WHERE store_number = '5116'
+ORDER BY zip_code;
+
+-- update zip code store 5116 from 50702 to 50208
+UPDATE iowa_drink_sales 
+SET zip_code = '50208'
+WHERE store_name = '5116' AND zip_code = '50702';
