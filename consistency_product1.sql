31c0a52de4d49b80e0ea9d47b235430757addc6a
magusafr <40602197+magusafr@users.noreply.github.com>
2022-09-25 20:01:38 +0700

    Create consistency_product1.sql

---
+++ consistency_product1.sql
@@ -0,0 +1,16 @@
+-- Product consistency
+SELECT 
+  item_number,
+  COUNT(DISTINCT(item_description)) AS total_item_description,
+  COUNT(DISTINCT(category_name)) AS total_category_name,
+  COUNT(DISTINCT(category)) AS total_category_number
+FROM 
+	iowa_drink_sales
+GROUP BY
+	item_number
+HAVING 
+	COUNT(DISTINCT(item_description)) > 1 OR
+	COUNT(DISTINCT(category_name)) > 1 OR
+	COUNT(DISTINCT(category)) > 1
+ORDER BY 
+	total_item_description DESC, item_number;
